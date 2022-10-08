07f19b15549617f50d21eedab9ec7950b90cf601
Xenion1987 <39803750+Xenion1987@users.noreply.github.com>
2022-10-08 12:27:53 +0200

    var for ports

--- matebook-x-pro-2021/manjaro/i3/home/ownScripts/setDisplays_choose_matebook.sh
+++ matebook-x-pro-2021/manjaro/i3/home/ownScripts/setDisplays_choose_matebook.sh
@@ -3,7 +3,11 @@
 # https://github.com/Chrysostomus/bmenu
 
 BACKGROUND_IMAGE=/usr/share/backgrounds/blackcat.jpg
-
+PORT_0=eDP1
+PORT_A=DP3
+PORT_B=DP1
+PORT_C=DP2
+PORT_D=DP4
 function get_port_names() {
     while read -r PORT_NAME CONNECT_STATUS; do
         PORTS_CONNECTED+=("${PORT_NAME}" "${CONNECT_STATUS}")
@@ -13,7 +17,7 @@ function get_display_resolutions() {
     set -x
     for ((i = 0; i < ${#PORTS_CONNECTED[@]}; i++)); do
         #e=$((i + 2))
-        xrandr | awk '/start1/{flag=1}/DP4/{flag=0}flag' start1="${PORTS_CONNECTED[${i}]}"
+        xrandr | awk '/start1/{flag=1}/"${PORT_D}"/{flag=0}flag' start1="${PORTS_CONNECTED[${i}]}"
         #echo -e " [${COUNT}] ${PORTS_CONNECTED[${i}]}"
         ((++i))
     done
@@ -29,7 +33,7 @@ function print_displays() {
 }
 function main {
     clear
-    cat <<_EOF
+    cat <<ENDOFFILE
      ┌───┐
      └───┘
       [1]
@@ -44,59 +48,60 @@ function main {
      └──┘
       [3]
 ---------------
-#  ┌────┐┌────┐
-#  └DP-0┘└DP-1┘
-#       [4]
-# ---------------
-#  ┌────┐┌────┐
-#  └DP-0┘└DP-1┘
-#      ┌──┐
-#      └──┘
-#       [5]
-# ---------------
-#       ┌──┐
-#      ┌──┐┘
-#      └──┘
-#       [6]
-# ---------------
-_EOF
+ENDOFFILE
+    #  ┌────┐┌────┐
+    #  └DP-0┘└DP-1┘
+    #       [4]
+    # ---------------
+    #  ┌────┐┌────┐
+    #  └DP-0┘└DP-1┘
+    #      ┌──┐
+    #      └──┘
+    #       [5]
+    # ---------------
+    #       ┌──┐
+    #      ┌──┐┘
+    #      └──┘
+    #       [6]
+    # ---------------
+
     read -rs -n1 CHOICE
 
     case $CHOICE in
     1)
-        xrandr --output eDP1 --primary --mode 1920x1280 --rotate normal \
-            --output DP1 --off \
-            --output DP2 --off \
-            --output DP3 --off \
-            --output DP4 --off
+        xrandr --output "${PORT_0}" --primary --mode 1920x1280 --rotate normal \
+            --output "${PORT_A}" --off \
+            --output "${PORT_B}" --off \
+            --output "${PORT_C}" --off \
+            --output "${PORT_D}" --off
         feh --image-bg "#111920" --bg-fill ${BACKGROUND_IMAGE}
         ;;
     2)
-        xrandr --output eDP1 --primary --mode 1920x1280 --rotate normal \
-            --output DP1 --off \
-            --output DP2 --off \
-            --output DP3 --off \
-            --output DP4 --off
+        xrandr --output "${PORT_0}" --primary --mode 1920x1280 --rotate normal \
+            --output "${PORT_A}" --off \
+            --output "${PORT_B}" --off \
+            --output "${PORT_C}" --off \
+            --output "${PORT_D}" --off
         sleep 1
-        xrandr --output DP1 --primary --mode 3440x1440 --rotate normal \
-            --output eDP1 --off \
-            --output DP2 --off \
-            --output DP3 --off \
-            --output DP4 --off
+        xrandr --output "${PORT_A}" --primary --mode 3440x1440 --rotate normal \
+            --output "${PORT_0}" --off \
+            --output "${PORT_B}" --off \
+            --output "${PORT_C}" --off \
+            --output "${PORT_D}" --off
         feh --image-bg "#111920" --bg-fill ${BACKGROUND_IMAGE}
         ;;
     3)
-        xrandr --output eDP1 --primary --mode 1920x1280 --rotate normal \
-            --output DP1 --off \
-            --output DP2 --off \
-            --output DP3 --off \
-            --output DP4 --off
+        xrandr --output "${PORT_0}" --primary --mode 1920x1280 --rotate normal \
+            --output "${PORT_A}" --off \
+            --output "${PORT_B}" --off \
+            --output "${PORT_C}" --off \
+            --output "${PORT_D}" --off
         sleep 1
-        xrandr --output DP1 --primary --mode 3440x1440 --rotate normal \
-            --output eDP1 --mode 1920x1280 --below DP1 --rotate normal \
-            --output DP2 --off \
-            --output DP3 --off \
-            --output DP4 --off
+        xrandr --output "${PORT_A}" --primary --mode 3440x1440 --rotate normal \
+            --output "${PORT_0}" --mode 1920x1280 --below "${PORT_A}" --rotate normal \
+            --output "${PORT_B}" --off \
+            --output "${PORT_C}" --off \
+            --output "${PORT_D}" --off
         feh \
             --image-bg "#111920" --bg-fill ${BACKGROUND_IMAGE} \
             --image-bg "#111920" --bg-fill ${BACKGROUND_IMAGE}
@@ -112,7 +117,7 @@ _EOF
     esac
 }
 
-# if main; then
-#     sleep 5
-#     i3-msg restart
-# fi
+if main; then
+    sleep 5
+    i3-msg restart
+fi
